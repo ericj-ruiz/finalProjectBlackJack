fa654d2db91652d050a345b91a77753e6192a5d0
eric ruiz <ericj.ruiz@outlook.com>
2021-10-30 13:55:06 -0700

    worked on view and controller

--- src/com/company/controllers/Decision.java
+++ src/com/company/controllers/Decision.java
@@ -1,13 +1,4 @@
 package com.company.controllers;
-import javax.imageio.IIOException;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Decision extends GetDeck{
     private String decision;
--- src/com/company/views/CmdLineView.java
+++ src/com/company/views/CmdLineView.java
@@ -1,4 +1,19 @@
 package com.company.views;
 
 public class CmdLineView {
+
+
+    public void promptContinue(){
+        System.out.println("Would you like to continue: Yes or No?");
+    }
+
+    public void greeting() {
+        System.out.println("Hello would you like to play?");
+    }
+    public void promptBet(){
+        System.out.println("Place your bets:");
+    }
+    public void promptDecision(){
+        System.out.println("What decision would you like to make?");
+    }
 }
