fa7b588f3b841ed37dbbfaf9a97e048233bf7d3e
eric ruiz <ericj.ruiz@outlook.com>
2021-12-16 17:07:05 -0800

    finished project

--- src/com/company/controllers/Main.java
+++ src/com/company/controllers/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static int bankRoll;
     public static int pot;
     public static String deck[];
+    public static int loopcheck;
 
 
 
@@ -22,12 +23,9 @@ public class Main {
     bankRoll=input.nextInt();
 
     while(cont ==1){
-        //break if bankroll is not enough
         view.greeting();
         pot= input.nextInt();
         if(pot<=bankRoll) {
-            bankRoll -= pot;
-            //if()
             int uTotal = 0;
             int hTotal = 0;
             int user1 = deal();
@@ -37,13 +35,15 @@ public class Main {
             int house1 = deal();
             hTotal = house1;
             view.houseCards(house1);
-
+            loopcheck=0;
             int decision = view.promptDecision();
             do {
-                uTotal = decision(decision, uTotal);
+                uTotal = decision(decision, uTotal,user1,user2);
 
                 if (uTotal > 21) {
                     view.showBust();
+                    bankRoll-=pot;
+                    view.lose(bankRoll,pot);
                     break;
                 }
                 if (decision != 2)
@@ -52,23 +52,45 @@ public class Main {
             } while (decision != 2);
             if (uTotal <= 21) {
                 hTotal = dealerDeal(hTotal, house1);
-                if (hTotal < 21) {//show totals
-                    if (uTotal == hTotal)
-                        view.tie();
-                    else if (uTotal > hTotal)
-                        view.win();
-                    else
-                        view.lose();
+                if (hTotal <= 21) {
+                    if (uTotal == hTotal) {
+                        view.totals(hTotal, uTotal);
+                        view.tie(bankRoll,pot);
+
+                    }
+                    else if (uTotal > hTotal) {
+                        view.totals(hTotal,uTotal);
+                        bankRoll+=pot;
+                        view.win(bankRoll,pot);
+                    }
+                    else {
+                        view.totals(hTotal,uTotal);
+                        bankRoll-=pot;
+                        view.lose(bankRoll,pot);
+                    }
+                }
+                else if(hTotal>21){
+                    view.totals(hTotal,uTotal);
+                    bankRoll+=pot;
+                    view.win(bankRoll,pot);
+                }
 
                 }
-            }
-            //add or subtract winnings
+
             cont = view.promptContinue();
         }
         else
         {
-            view.noMoney();
-            cont=0;
+            if(bankRoll==0){
+                view.noMoney();
+                cont=0;
+            }
+            else{
+                view.notEnuffMoney(bankRoll);
+                cont = view.promptContinue();
+            }
+
+
         }
     }
 
@@ -110,15 +132,37 @@ public class Main {
         return n;
     }
 
-    public static int decision(int decis, int total) {
+    public static int decision(int decis, int total,int u1, int u2) {
         CmdLineView view = new CmdLineView();
         int t=0;
 
         if (decis == 1) {
             int user3 = deal();
+            int check =total;
+            check+=user3;
+            if(check>21 && loopcheck==0)//once entered once it should stop
+            {
+                if(u1==11||u2==11){
+                    total-=10;
+                    loopcheck=1;
+                }
+//check once  with user 1 or 11last hit and stand to see if the total is accurate
+            }
+           // if(u1==11||u2==11)
+               // loopcheck=1;
             total = view.hitCards(user3,total);
             t=total;
-            view.showTotal(t);
+
+               // if (loopcheck==1&& total<21) {
+                  //  view.showTotal2(t, t - 10);
+                  //  if(check<21)
+                     //   loopcheck=0;
+                   // else
+                    //    loopcheck=2;
+               // }
+                //else
+                    view.showTotal(t);
+
         }
         else if(decis== 2){
             t=total;
@@ -130,17 +174,34 @@ public class Main {
 
     public static int dealerDeal(int total,int house1){
         CmdLineView view = new CmdLineView();
+
+        //check to see if hit card 11 changes
         int house2=deal();
         total+=house2;
+        view.showHTotal(total,house2);
         while(total <17) {
             int house3 = deal();
             total+=house3;
+            if(total>21)
+            {
+                if(house1==11) {
+                    total -= 10;
+                    house1=1;
+                }
+                if(house2==11) {
+                    total -= 10;
+                    house2=1;
+                }
+                if(house3==11) {
+                    total -= 10;
+                    house3=1;
+                }
+            }
+            view.showHTotal(total,house3);
             if(total > 21)
                 view.houseBust();
         }
-        //check first card if 1 or 11
-        //add until 17 or bust
-        //show totals
+
         return total;
     }
 
--- src/com/company/views/CmdLineView.java
+++ src/com/company/views/CmdLineView.java
@@ -11,6 +11,7 @@ public class CmdLineView {
 
 
     public int promptContinue(){
+        System.out.println(".............................................................");
         System.out.println("Would you like to continue: 1 = Yes or 0 = No?");
         Scanner con = new Scanner(System.in);
         int in =con.nextInt();
@@ -30,6 +31,7 @@ public class CmdLineView {
 
 
     public int userCards(int user1,int user2,int uTotal){
+        System.out.println("-------------------");
         if(user1 ==11) {
             System.out.println("User Card: 1 or 11");
             uTotal = user1;
@@ -49,7 +51,19 @@ public class CmdLineView {
             System.out.println("User Card: " + user2);
             uTotal += user2;
         }
-        System.out.println("User Total: "+uTotal );
+        System.out.println("-------------------");
+
+        if(user1==11 || user2==11){
+            int t;
+            if(user1==11 && user2==11)
+                t=22;
+            else
+                t=uTotal-10;
+            System.out.println("User Total: "+uTotal+" or "+t);
+
+        }
+        else
+            System.out.println("User Total: "+uTotal );
         return uTotal;
 
     }
@@ -76,28 +90,49 @@ public class CmdLineView {
 
     }
 
+
     public void showTotal(int t){
-        System.out.println("Total: "+t);
+        System.out.println("-------------------");
+        System.out.println("User Total: "+t);
+        System.out.println("-------------------");
+    }
+    public void showTotal2(int t,int t2){
+        System.out.println("-------------------");
+        System.out.println("User Total: "+t +" or "+t2);
+        System.out.println("-------------------");
+    }
+    public void showHTotal(int t,int card){
+        System.out.println("Hit Card: "+card);
+        System.out.println("-------------------");
+        System.out.println("House Total: "+t);
+        System.out.println("-------------------");
     }
     public void showBust(){
         System.out.println("You have busted! You LOST!");
     }
     public void houseBust(){
-        System.out.println("House has busted! You WIN!");
+        System.out.println("House has busted!");
 
     }
-    public void tie(){
-        System.out.println("You tied with the house");
+    public void tie(int br,int bet){
+        System.out.println("You tied with the house." + " Your bankroll total is still: $"+br);
     }
-    public void win(){
-        System.out.println("You Win!");
+    public void win(int br,int bet){
+        System.out.println("You Win! $" + bet + " Your bankroll total is now: $"+br);
     }
-    public void lose(){
-        System.out.println("You Lost!");
+    public void lose(int br, int bet){
+        System.out.println("You Lost! $" + bet + " Your bankroll total is now: $"+br);
+    }
+    public void notEnuffMoney(int br){
+        System.out.println("You don't have enough $$$ in your Bankroll to make that bet, You have $"+br +" in your bankroll make the reasonable bet.");
+    }
+    public void totals(int ht, int ut){
+        System.out.println("House Total: "+ht +" VS User Total: "+ut);
     }
     public void noMoney(){
-        System.out.println("You don't have enough $$$ in your Bankroll");
+        System.out.println("You don't have any $$$, YOU ARE BROKE GO MAKE SOME MONEY THEN COME BACK HAVE A NICE DAY!");
     }
+
 
 
 }
